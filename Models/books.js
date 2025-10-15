a77e245dc956a960910765546d4a279fad1f355a
Exwhybaba <seyeoyelayo@gmail.com>
2025-10-15 07:53:33 +0100

    Overwrite remote repository with local version

---
+++ Models/books.js
@@ -0,0 +1,17 @@
+const { mongoose } = require('../db');
+const { Schema } = mongoose;
+
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: { type: String, required: true },
+    published_year: { type: Number, required: true },
+    price: { type: Number, required: true },
+    in_stock: { type: Boolean, default: true },
+    pages: { type: Number, required: true },
+    publisher: { type: String, required: true }
+}, { timestamps: true });
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = { Book };
